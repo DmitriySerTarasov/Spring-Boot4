20909b6373ef2e719650b8f09d12f414deb83f90
Дмитрий <tds1991j@gmail.com>
2025-07-14 11:26:05 +0700

    Update JavaConfig.java

--- Spring-Boot3/src/main/java/com/example/Spring_Boot3/config/JavaConfig.java
+++ Spring-Boot3/src/main/java/com/example/Spring_Boot3/config/JavaConfig.java
@@ -1,23 +1,24 @@
 package com.example.Spring_Boot3.config;
 
-
 import com.example.Spring_Boot3.profile.DevProfile;
 import com.example.Spring_Boot3.profile.ProductionProfile;
 import com.example.Spring_Boot3.profile.SystemProfile;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.context.annotation.Configuration;
 
-@ConditionalOnProperty
+@Configuration
 public class JavaConfig {
+
     @Bean
-    public SystemProfile DevProfile() {
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true", matchIfMissing = false)
+    public SystemProfile devProfile() {
         return new DevProfile();
     }
 
     @Bean
-    public SystemProfile ProdProfile() {
+    @ConditionalOnProperty(value = "netology.profile.prod", havingValue = "true", matchIfMissing = false)
+    public SystemProfile prodProfile() {
         return new ProductionProfile();
     }
 }
